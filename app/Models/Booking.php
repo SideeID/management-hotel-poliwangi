f6d4ae7deaa64009778e4f25cb910124986b71e8
SideeID <sandipratama164026@gmail.com>
2024-06-23 22:42:43 +0700

    chore: Add fillable fields and relationships to Booking model

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Booking extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'reservations_id',
+        'booking_date',
+        'total_harga',
+        'total_tamu',
+    ];
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class, 'reservations_id');
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class, 'bookings_id');
+    }
 }
